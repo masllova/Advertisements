3387aecb2918faf76527835b93be7e6baff53c61
alexa <allexa.msv@gmail.com>
2023-08-27 13:02:10 +0300

    Initial Commit

---
+++ AvitoTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTestProject
+//
+//  Created by Александра Маслова on 27.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
